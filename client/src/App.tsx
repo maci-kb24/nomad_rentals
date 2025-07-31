13bc9b09fbb60208c57b016a15449260177001b5
maci-kb24 <macikb8@yahoo.com>
2025-07-31 23:45:54 +0200

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/ui/button'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline text-center text-blue-500">
+          Hello world!
+          <Button>
+            Click Me
+          </Button>
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
